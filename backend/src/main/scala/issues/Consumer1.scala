a4ec30d8160561445a8b86d7a7b874962474f4dc
twr143 <twr143@gmail.com>
2019-12-30 22:15:36 +0300

    topic example, 1

---
+++ backend/src/main/scala/issues/Consumer1.scala
@@ -0,0 +1,61 @@
+package issues
+
+import cats.implicits._
+import cats.effect.concurrent.Deferred
+import monix.eval.Task
+import fs2._
+import fs2.concurrent.Topic
+
+import scala.concurrent.duration._
+import monix.execution.Scheduler.Implicits.global
+
+/**
+  * Created by Ilya Volynin on 30.12.2019 at 16:56.
+  */
+object Consumer1 {
+
+  def main(args: Array[String]): Unit = {
+    implicit class Runner[A](s: Stream[Task, A]) {
+       def yolo(): Unit = s.compile.drain.runSyncUnsafe()
+       def yoloV: Vector[A] = s.compile.toVector.runSyncUnsafe()
+     }
+   
+     // put("hello").to[F]
+     def put[A](a: A): Task[Unit] = Task.now(println(s"$a csubscribers connected"))
+   
+     def prog1(): Unit =
+       Stream
+         .eval {
+           (Topic[Task, String]("initial"), Deferred[Task, Unit]).tupled
+         }
+         .flatMap {
+           case (topic, stop) =>
+             val kafkaConsumer = Stream
+                 .awakeEvery[Task](1.seconds).zipWithIndex
+                 .map(m => s"msg${m._2}")
+                 .repeat
+                 .take(3) ++ Stream.eval(stop.complete(())).drain
+   
+             val subscribers = topic.subscribers.evalTap(put)
+             val subscriber1           = topic.subscribe(5)
+               .evalTap[Task](message => Task { println(s"subscriber1: $message") })
+                val subscriber2           = topic.subscribe(5)
+                  .evalTap[Task](message => Task { println(s"subscriber2: $message") }).map(_.some)
+                  .unNoneTerminate.interruptWhen(stop.get.attempt)
+
+             val producer = kafkaConsumer.broadcastTo(topic.publish)
+             val consumer = Stream.sleep_[Task](200.millis) ++ topic
+                 .subscribe(10)
+                 .showLinesStdOut
+                 .interruptWhen(stop.get.attempt)
+   
+             consumer concurrently Stream(
+               producer,
+               subscribers,
+               subscriber1,
+               subscriber2
+             ).parJoinUnbounded
+         }.yolo()
+   prog1()
+  }
+}
--- backend/src/main/scala/issues/Stream1.scala
+++ backend/src/main/scala/issues/Stream1.scala
@@ -1,18 +1,13 @@
 package issues
-import cats.implicits._
 import monix.eval.Task
-
+import fs2._
 import scala.concurrent.duration._
 import monix.execution.Scheduler.Implicits.global
-import monix.eval.instances.CatsConcurrentForTask
-
-import scala.concurrent.Await
 
 /**
   * Created by Ilya Volynin on 25.12.2019 at 15:28.
   */
 object Stream1 {
-  import fs2._
 
   def main(args: Array[String]): Unit = {
     //    val str = Stream(0).repeat.zipWithIndex.map(c => (c._2 * c._2).toInt).take(10)
@@ -20,21 +15,33 @@ object Stream1 {
     //    val running = str.evalMap(IO(_)).compile.toList.unsafeRunSync()
     //    println(l)
     //    println(running)
-        val l2 = Stream.range(1, 10)
-      .metered[Task](1 second)
-      .covary[Task]
-      .onFinalize(Task.now(println("finalized"))).compile.toList.runSyncUnsafe()
-
-//      .andThen{case scala.util.Success(value) => println(s"list: $value")}//(either => either.fold(tw => println(tw.getMessage),lst => println(s"list: $lst")))
-//    scala.concurrent.Await.result(l2, 13.seconds)
-
+    //        val l2 = Stream.range(1, 10)
+    //      .metered[Task](1 second)
+    //      .covary[Task]
+    //      .onFinalize(Task.now(println("finalized"))).compile.toList.runSyncUnsafe()
+    //      .andThen{case scala.util.Success(value) => println(s"list: $value")}//(either => either.fold(tw => println(tw.getMessage),lst => println(s"list: $lst")))
+    //    scala.concurrent.Await.result(l2, 13.seconds)
     //    Stream(1,2,3,4).broadcast[Task].map { worker =>
     //             worker.evalMap { o => Task.now(println("1:" + o.toString)) }
     //           }.take(3).parJoinUnbounded.compile.drain.runSyncUnsafe()
-//    val r = Stream(1, 2, 3, 4).balance[Task](1).map { worker =>
-//      worker.map { i => println(s"thread id: ${Thread.currentThread().getId}, i =$i"); i.toString }
-//    }.take(1)//number of threads
-//      .parJoinUnbounded.compile.to[Set].runSyncUnsafe()
-//    println(r)
+    //    val r = Stream(1, 2, 3, 4).balance[Task](1).map { worker =>
+    //      worker.map { i => println(s"thread id: ${Thread.currentThread().getId}, i =$i"); i.toString }
+    //    }.take(1)//number of threads
+    //      .parJoinUnbounded.compile.to[Set].runSyncUnsafe()
+    //    println(r)
+    //    val s2 = Stream.range(100, 110).metered[Task](15.millis).covary[Task]
+    //    val s3 = Stream(false,true).metered[Task](10.millis).covary[Task]
+//        val s1 = Stream.range(1, 10).metered[Task](10.millis).interruptWhen(s3).covary[Task]
+    //
+    //    s1.merge(s2).compile.toList.runSyncUnsafe()
+
+//    var border = 8
+//
+//    def flag(index: Long) = if (index < border) false else true
+//
+//    val res = Stream(false).repeat.zipWithIndex.map { pair => if (pair._2 == 3) border = 6; flag(pair._2) }.metered[Task](1000.millis).covary[Task].take(10).compile.toList.runSyncUnsafe()
+//    println(res)
+//    Stream.range(1,10).reduce(0)(_ + _).toList
+
   }
 }
