d177be863f50aefc2b51723150ba8c82d367fc9e
twr143 <twr143@gmail.com>
2021-01-08 22:18:10 +0300

    versions update

--- backend/src/main/scala/issues/Consumer1.scala
+++ backend/src/main/scala/issues/Consumer1.scala
@@ -16,46 +16,52 @@ object Consumer1 {
 
   def main(args: Array[String]): Unit = {
     implicit class Runner[A](s: Stream[Task, A]) {
-       def yolo(): Unit = s.compile.drain.runSyncUnsafe()
-       def yoloV: Vector[A] = s.compile.toVector.runSyncUnsafe()
-     }
+      def yolo(): Unit = s.compile.drain.runSyncUnsafe()
+      def yoloV: Vector[A] = s.compile.toVector.runSyncUnsafe()
+    }
 
-     // put("hello").to[F]
-     def put[A](a: A): Task[Unit] = Task.now(println(s"$a csubscribers connected"))
+    // put("hello").to[F]
+    def put[A](a: A): Task[Unit] = Task.now(println(s"$a csubscribers connected"))
 
-     def prog1(): Unit =
-       Stream
-         .eval {
-           (Topic[Task, String]("initial"), Deferred[Task, Unit]).tupled
-         }
-         .>>= {
-           case (topic, stop) =>
-             val kafkaConsumer = Stream
-                 .awakeEvery[Task](1.seconds).zipWithIndex
-                 .map(m => s"msg${m._2}")
-                 .repeat
-                 .take(3) ++ Stream.eval(stop.complete(())).drain
+    def prog1(): Unit =
+      Stream
+        .eval {
+          (Topic[Task, String]("initial"), Deferred[Task, Unit]).tupled
+        }
+        .>>= {
+          case (topic, stop) =>
+            val kafkaConsumer = Stream
+              .awakeEvery[Task](1.seconds)
+              .zipWithIndex
+              .map(m => s"msg${m._2}")
+              .repeat
+              .take(3) ++ Stream.eval(stop.complete(())).drain
 
-             val subscribers = topic.subscribers.evalTap(put)
-             val subscriber1           = topic.subscribe(5)
-               .evalTap[Task](message => Task { println(s"subscriber1: $message") })
-                val subscriber2           = topic.subscribe(5)
-                  .evalTap[Task](message => Task { println(s"subscriber2: $message") }).map(_.some)
-                  .unNoneTerminate.interruptWhen(stop.get.attempt)
+            val subscribers = topic.subscribers.evalTap(put)
+            val subscriber1 = topic
+              .subscribe(5)
+              .evalTap(message => Task { println(s"subscriber1: $message") })
+            val subscriber2 = topic
+              .subscribe(5)
+              .evalTap(message => Task { println(s"subscriber2: $message") })
+              .map(_.some)
+              .unNoneTerminate
+              .interruptWhen(stop.get.attempt)
 
-             val producer = kafkaConsumer.through(topic.publish)
-             val consumer = Stream.sleep_[Task](200.millis) ++ topic
-                 .subscribe(10)
-                 .showLinesStdOut
-                 .interruptWhen(stop.get.attempt)
+            val producer = kafkaConsumer.through(topic.publish)
+            val consumer = Stream.sleep_[Task](200.millis) ++ topic
+              .subscribe(10)
+              .showLinesStdOut
+              .interruptWhen(stop.get.attempt)
 
-             consumer concurrently Stream(
-               producer,
-               subscribers,
-               subscriber1,
-               subscriber2
-             ).parJoinUnbounded
-         }.yolo()
-   prog1()
+            consumer concurrently Stream(
+              producer,
+              subscribers,
+              subscriber1,
+              subscriber2
+            ).parJoinUnbounded
+        }
+        .yolo()
+    prog1()
   }
 }
